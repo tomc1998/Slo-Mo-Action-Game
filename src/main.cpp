89b11616eb27cde83981e50107d282e917485eaa
pomchee2 <joe.cheng2001@gmail.com>
2017-12-26 22:55:00 +0000

    Actually moved main loop to engine.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,6 @@
 int main(int argc, char** argv) {
 
   Engine* engine = new Engine;
-  while(true) {
-    engine->engine_go();
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwSwapBuffers(engine->window);
-    if (glfwWindowShouldClose(engine->window)){
-      break;
-    }
+  engine->engine_go();
   }
 }
