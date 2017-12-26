679be689ba443a16bc7f61044a93b7af14b51784
Thomas Cheng <thomascheng1998@googlemail.com>
2017-12-26 01:53:11 +0000

    Added common number typedefs

---
+++ src/engine/typedefs.hpp
@@ -0,0 +1,15 @@
+/** A list of convenience typedefs for easier typing */
+#include <cstdint>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+// Just guessing these are 32-bit and 64-bit here, pretty sure though
+typedef float f32;
+typedef double f64;
