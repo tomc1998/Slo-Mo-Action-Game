73a7ef87601c6d8b5d5d00547585022238717392
pomchee2 <joe.cheng2001@gmail.com>
2017-12-27 13:21:26 +0000

    made playercontrolledcomponent faster and easier to read

--- src/engine/system/player_controlled.cpp
+++ src/engine/system/player_controlled.cpp
@@ -7,24 +7,28 @@ class SystemPlayerControlled: public System {
     void handle_components(ECS* ecs, InputState* input_state ) {
       for (u32 ii=0; ii < ecs->comp_game_entity.size(); ii++) {
         for (u32 jj=0; jj < ecs->comp_player_controlled.size(); ii++) {
-          if (ecs->comp_player_controlled[jj].entity_id == ecs->comp_game_entity[ii].entity_id) {
-            if (input_state->move_up >= 0) {
-              ecs->comp_game_entity[ii].acc.y = ecs->comp_game_entity[ii].acc.y - ecs->comp_player_controlled[jj].force_to_apply/ecs->comp_game_entity[ii].mass * input_state->move_up;
-            }
-
-            if (input_state->move_right >= 0) {
-              ecs->comp_game_entity[ii].acc.x = ecs->comp_game_entity[ii].acc.x + ecs->comp_player_controlled[jj].force_to_apply/ecs->comp_game_entity[ii].mass * input_state->move_right;
-            }
-
-            if (input_state->move_down >= 0) {
-              ecs->comp_game_entity[ii].acc.y = ecs->comp_game_entity[ii].acc.y + ecs->comp_player_controlled[jj].force_to_apply/ecs->comp_game_entity[ii].mass * input_state->move_down;
-            }
-
-            if (input_state->move_left >= 0) {
-              ecs->comp_game_entity[ii].acc.x = ecs->comp_game_entity[ii].acc.y - ecs->comp_player_controlled[jj].force_to_apply/ecs->comp_game_entity[ii].mass * input_state->move_left;
-            }
+          if (ecs->comp_player_controlled[jj].entity_id != ecs->comp_game_entity[ii].entity_id) {
+            continue;
+          }
+          Vec2* acc = &ecs->comp_game_entity[ii].acc;
+          f32 force_to_apply = ecs->comp_player_controlled[jj].force_to_apply;
+          f32 mass = ecs->comp_game_entity[ii].mass;
+          if (input_state->move_up >= 0) {
+            acc->y = acc->y - force_to_apply/mass * input_state->move_up;
           }
 
+          if (input_state->move_right >= 0) {
+            acc->x = acc->x + force_to_apply/mass * input_state->move_right;
+          }
+
+          if (input_state->move_down >= 0) {
+            acc->y = acc->y + force_to_apply/mass * input_state->move_down;
+          }
+
+          if (input_state->move_left >= 0) {
+            acc->x = acc->x - force_to_apply/mass * input_state->move_left;
+          }
+          break;
         } 
       }
     }
