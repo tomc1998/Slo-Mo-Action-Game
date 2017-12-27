7f2e043e4289e44d2154f84f7c9b1713f9123ddc
Thomas Cheng <thomascheng1998@googlemail.com>
2017-12-27 15:17:47 +0000

    Added projection matrix uniform to scale drawing to screen size

--- assets/glsl/vert.glsl
+++ assets/glsl/vert.glsl
@@ -1,11 +1,13 @@
 #version 120
 
+uniform mat4 proj_mat;
+
 attribute vec3 pos;
 attribute vec4 col;
 
 varying vec4 v_col;
 
 void main() {
-  gl_Position = vec4(pos, 1.0);
+  gl_Position = proj_mat * vec4(pos, 1.0);
   v_col = col;
 }
