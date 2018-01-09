fb1efb32fddf482031eaa9e9992f5d34110a85b7
Thomas Cheng <thomascheng1998@googlemail.com>
2018-01-09 22:23:35 +0000

    Fixed issues wit previous game entity renderer commit

--- src/engine/system/game_entity_renderer.cpp
+++ src/engine/system/game_entity_renderer.cpp
@@ -23,18 +23,16 @@ public:
 
     for (u32 ii = 0; ii < ecs->comp_animation.size(); ii++) {
       CompAnimation &a = ecs->comp_animation[ii];
-      CompGameEntity &entity = ecs->find_comp_game_entity_with_id(a.entity_id);
+      CompGameEntity &entity = *ecs->find_comp_game_entity_with_id(a.entity_id);
       paint_controller->draw_animation(a.anim, a.updates, entity.pos.x,
                                        entity.pos.y, 16.0, 16.0, entity.rot,
                                        &white);
     }
     for (u32 ii = 0; ii < ecs->comp_sprite.size(); ii++) {
       CompSprite &s = ecs->comp_sprite[ii];
-      CompGameEntity &entity = ecs->find_comp_game_entity_with_id(s.entity_id);
+      CompGameEntity &entity = *ecs->find_comp_game_entity_with_id(s.entity_id);
       paint_controller->draw_image(s.tex, entity.pos.x, entity.pos.y, 16.0f,
                                    16.0f, entity.rot, &white);
     }
   }
-}
-}
-;
+};
