f8a234d56bd32859372bb4ce13a3aedfc6f6aaee
Thomas Cheng <thomascheng1998@googlemail.com>
2018-01-09 17:57:32 +0000

    Hugely optimised batch::buffer

--- src/engine/renderer/batch.cpp
+++ src/engine/renderer/batch.cpp
@@ -3,10 +3,7 @@
 Batch::Batch(u64 _tex) : tex(_tex) {}
 
 void Batch::buffer(Vertex *_vertices, size_t num_vertices) {
-  vertices.reserve(vertices.size() + num_vertices);
-  for (size_t ii = 0; ii < num_vertices; ++ii) {
-    vertices.push_back(_vertices[ii]);
-  }
+  vertices.insert(vertices.end(), _vertices, _vertices + num_vertices);
 }
 
 void Batch::clear() { vertices.clear(); }
